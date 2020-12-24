854726f3d497a97decec726c26aa9bf8b6a2a914
Stacy Potten <stacylynnaz@gmail.com>
2020-12-23 18:30:32 -0700

    Add Vue

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    };
+  },
+});
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
