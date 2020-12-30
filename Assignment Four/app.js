0388066bf0c36c929b9bcece6fd1f29223d91eb5
Stacy Potten <stacylynnaz@gmail.com>
2020-12-29 19:03:11 -0700

    Add inline styles

--- Assignment Four/app.js
+++ Assignment Four/app.js
@@ -2,9 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: '',
+      paragraphIsVisible: true,
+      inputBackgroundColor: '',
     };
   },
-  methods: {},
+  computed: {
+    paraClasses() {
+      return {
+        user1: this.inputClass === 'user1',
+        user2: this.inputClass === 'user2',
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible,
+      };
+    },
+  },
+  methods: {
+    toggleParagraphVisibility() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+  },
 });
 
 app.mount('#assignment');
