52b9220137db9757e61c5b648b20778d2cd1b4a3
Stacy Potten <stacylynnaz@gmail.com>
2020-12-30 16:25:12 -0700

    Limit special attacks

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -28,6 +33,7 @@ const app = Vue.createApp({
       this.playerHealth -= attackValue;
     },
     specialAttackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
