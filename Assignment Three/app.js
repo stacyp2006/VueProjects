a1b85c8389e3601e6d681564e4ecf383d58f9751
Stacy Potten <stacylynnaz@gmail.com>
2020-12-29 17:07:13 -0700

    Add button functionality to add buttons

--- Assignment Three/app.js
+++ Assignment Three/app.js
@@ -1,6 +1,23 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      counter: 0,
+      result: '',
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    // findResult() {
+    //   if (this.counter < 37) {
+    //     this.result = 'Not there yet';
+    //   } else if (this.counter > 37) {
+    //     this.result = 'Too much!';
+    //   } else if (this.counter === 37) {
+    //     this.result = 37;
+    //   }
+    },
   },
 });
 
