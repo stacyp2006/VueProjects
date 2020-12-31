78eb31e1de147f1c291c1efea4b3dfdb1abdc7dd
Stacy Potten <stacylynnaz@gmail.com>
2020-12-30 21:15:25 -0700

    unmount lifecycle

--- Behind the Scenes/app.js
+++ Behind the Scenes/app.js
@@ -32,9 +32,19 @@ const app = Vue.createApp({
   updated() {
     console.log('updated()');
   },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  },
 });
 
 app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
   template: `
