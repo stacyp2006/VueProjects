2a096d5ef731c25fc04246fa3a842b0addc745d1
Stacy Potten <stacylynnaz@gmail.com>
2020-12-31 15:14:15 -0700

    Add component to html

--- Components/app.js
+++ Components/app.js
@@ -20,9 +20,27 @@ const app = Vue.createApp({
 });
 
 app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show'}} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone}}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
   data() {
     return {
       detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
     };
   },
   methods: {
