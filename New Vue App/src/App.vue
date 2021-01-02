699a6515e23f2be2112057b72e40fff2d22ada8f
Stacy Potten <stacylynnaz@gmail.com>
2021-01-01 22:24:20 -0700

    Add app.vue file

---
+++ New Vue App/src/App.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script>
+const app = {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 5678 991',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0789 9876 6765',
+          email: 'julie@localhost.com',
+        }
+      ]
+    }
+  }
+</script>
