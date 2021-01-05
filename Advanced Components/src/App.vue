7ca231322307a6756fbf99e24eb9d34f2cd31d2c
Stacy Potten <stacylynnaz@gmail.com>
2021-01-04 19:52:11 -0700

    Add CourseGoals component

--- Advanced Components/src/App.vue
+++ Advanced Components/src/App.vue
@@ -7,6 +7,7 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals></course-goals>
   </div>
 </template>
 
@@ -14,9 +15,10 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo },
+  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
   data() {
     return {
       activeUser: {
