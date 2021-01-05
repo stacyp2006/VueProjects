3b4331a42ca2dc65e9fa345916164f07348af104
Stacy Potten <stacylynnaz@gmail.com>
2021-01-04 19:09:30 -0700

    Locally register component

--- Advanced Components/src/App.vue
+++ Advanced Components/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <the-header></the-header>
+    <TheHeader />
     <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
@@ -11,7 +11,10 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: { TheHeader },
   data() {
     return {
       activeUser: {
