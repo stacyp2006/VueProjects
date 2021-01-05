d9d970c51683a5951c46f134cea57e1858b23ae8
Stacy Potten <stacylynnaz@gmail.com>
2021-01-04 20:19:49 -0700

    Add funtionality to Managegoals

--- Advanced Components/src/components/ManageGoals.vue
+++ Advanced Components/src/components/ManageGoals.vue
@@ -1,3 +1,20 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set goal</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        alert('Input must not be empty');
+      }
+    },
+  },
+};
+</script>
