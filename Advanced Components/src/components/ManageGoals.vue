d822cf6a6e80899f9744c679af75b6044308f091
Stacy Potten <stacylynnaz@gmail.com>
2021-01-04 20:30:59 -0700

    Add error handling

--- Advanced Components/src/components/ManageGoals.vue
+++ Advanced Components/src/components/ManageGoals.vue
@@ -3,17 +3,35 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid</h2>
+      <p>Please enter at least a few characters</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
   </div>
 </template>
 
 <script>
+import ErrorAlert from './ErrorAlert.vue';
+
 export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     setGoal() {
       const enteredValue = this.$refs.goal.value;
       if (enteredValue === '') {
-        alert('Input must not be empty');
+        this.inputIsInvalid = true;
       }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
